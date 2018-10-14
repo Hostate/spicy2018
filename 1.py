7a52db15cddcbf2fa480983c8a7e393368d13761
Zhaoqin Cai <czqin913@163.com>
2018-10-14 20:24:43 +0800

    change

--- 1.py
+++ 1.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 """
 This experiment was created using PsychoPy2 Experiment Builder (v1.90.1),
-    on 十月 14, 2018, at 20:17
+    on 十月 14, 2018, at 19:43
 If you publish work using this script please cite the PsychoPy publications:
     Peirce, JW (2007) PsychoPy - Psychophysics software in Python.
         Journal of Neuroscience Methods, 162(1-2), 8-13.
@@ -40,7 +40,7 @@ filename = _thisDir + os.sep + u'data/%s_%s_%s' % (expInfo['participant'], expNa
 # An ExperimentHandler isn't essential but helps with data saving
 thisExp = data.ExperimentHandler(name=expName, version='',
     extraInfo=expInfo, runtimeInfo=None,
-    originPath='D:\\spicy2018\\1.psyexp',
+    originPath=None,
     savePickle=True, saveWideText=True,
     dataFileName=filename)
 # save a log file for detail verbose info
@@ -53,7 +53,7 @@ endExpNow = False  # flag for 'escape' or other condition => quit the exp
 
 # Setup the Window
 win = visual.Window(
-    size=[1360, 768], fullscr=True, screen=0,
+    size=[800, 600], fullscr=False, screen=0,
     allowGUI=False, allowStencil=False,
     monitor='testMonitor', color=[-1,-1,-1], colorSpace='rgb',
     blendMode='avg', useFBO=True)
@@ -93,9 +93,9 @@ or_hot = visual.TextStim(win=win, name='or_hot',
     pos=(-0.85, 0.6), height=0.06, wrapWidth=None, ori=0, 
     color='white', colorSpace='rgb', opacity=1,
     depth=-2.0);
-image_train_1 = visual.ImageStim(
-    win=win, name='image_train_1',
-    image='sin', mask=None,
+image_1 = visual.ImageStim(
+    win=win, name='image_1',
+    image='photo\\\\beauty\\\\mn.png', mask=None,
     ori=0, pos=(0, 0), size=(0.7, 1.1),
     color=[1,1,1], colorSpace='rgb', opacity=1,
     flipHoriz=False, flipVert=False,
@@ -191,156 +191,124 @@ thisExp.nextEntry()
 # the Routine "wel" was not non-slip safe, so reset the non-slip timer
 routineTimer.reset()
 
-# set up handler to look after randomisation of conditions etc
-trials = data.TrialHandler(nReps=1, method='random', 
-    extraInfo=expInfo, originPath=-1,
-    trialList=data.importConditions('loop_train_1.xlsx'),
-    seed=None, name='trials')
-thisExp.addLoop(trials)  # add the loop to the experiment
-thisTrial = trials.trialList[0]  # so we can initialise stimuli with some values
-# abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
-if thisTrial != None:
-    for paramName in thisTrial:
-        exec('{} = thisTrial[paramName]'.format(paramName))
-
-for thisTrial in trials:
-    currentLoop = trials
-    # abbreviate parameter names if possible (e.g. rgb = thisTrial.rgb)
-    if thisTrial != None:
-        for paramName in thisTrial:
-            exec('{} = thisTrial[paramName]'.format(paramName))
+# ------Prepare to start Routine "trian_1"-------
+t = 0
+trian_1Clock.reset()  # clock
+frameN = -1
+continueRoutine = True
+# update component parameters for each repeat
+key_resp_2 = event.BuilderKeyResponse()
+# keep track of which components have finished
+trian_1Components = [beauty_left, ugly_right, or_hot, image_1, cross, key_resp_2]
+for thisComponent in trian_1Components:
+    if hasattr(thisComponent, 'status'):
+        thisComponent.status = NOT_STARTED
+theseKeys = []
+# -------Start Routine "trian_1"-------
+while continueRoutine:
+    # get current time
+    t = trian_1Clock.getTime()
+    frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+    # update/draw components on each frame
     
-    # ------Prepare to start Routine "trian_1"-------
-    t = 0
-    trian_1Clock.reset()  # clock
-    frameN = -1
-    continueRoutine = True
-    # update component parameters for each repeat
-    image_train_1.setImage(image)
-    key_resp_2 = event.BuilderKeyResponse()
-    # keep track of which components have finished
-    trian_1Components = [beauty_left, ugly_right, or_hot, image_train_1, cross, key_resp_2]
+    # *beauty_left* updates
+    if t >= 0.0 and beauty_left.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        beauty_left.tStart = t
+        beauty_left.frameNStart = frameN  # exact frame index
+        beauty_left.setAutoDraw(True)
+    
+    # *ugly_right* updates
+    if t >= 0.0 and ugly_right.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        ugly_right.tStart = t
+        ugly_right.frameNStart = frameN  # exact frame index
+        ugly_right.setAutoDraw(True)
+    
+    # *or_hot* updates
+    if t >= 0.0 and or_hot.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        or_hot.tStart = t
+        or_hot.frameNStart = frameN  # exact frame index
+        or_hot.setAutoDraw(True)
+    
+    # *image_1* updates
+    if t >= 0 and image_1.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        image_1.tStart = t
+        image_1.frameNStart = frameN  # exact frame index
+        image_1.setAutoDraw(True)
+    
+    # *cross* updates
+    if (len(theseKeys) > 0 and ((key_resp_2.keys != str("'e'")) or (key_resp_2.keys != "'e'"))  and cross.status == NOT_STARTED):
+        # keep track of start time/frame for later
+        cross.tStart = t
+        cross.frameNStart = frameN  # exact frame index
+        cross.setAutoDraw(True)
+    
+    # *key_resp_2* updates
+    if t >= 0.0 and key_resp_2.status == NOT_STARTED:
+        # keep track of start time/frame for later
+        key_resp_2.tStart = t
+        key_resp_2.frameNStart = frameN  # exact frame index
+        key_resp_2.status = STARTED
+        # keyboard checking is just starting
+        win.callOnFlip(key_resp_2.clock.reset)  # t=0 on next screen flip
+        event.clearEvents(eventType='keyboard')
+    if key_resp_2.status == STARTED:
+        theseKeys = event.getKeys()
+        
+        # check for quit:
+        if "escape" in theseKeys:
+            endExpNow = True
+        if len(theseKeys) > 0:  # at least one key was pressed
+            key_resp_2.keys = theseKeys[-1]  # just the last key pressed
+            key_resp_2.rt = key_resp_2.clock.getTime()
+            # was this 'correct'?
+            if (key_resp_2.keys == str("'e'")) or (key_resp_2.keys == "'e'"):
+                key_resp_2.corr = 1
+            else:
+                key_resp_2.corr = 0
+            # a response ends the routine
+            continueRoutine = False
+    
+    # check if all components have finished
+    if not continueRoutine:  # a component has requested a forced-end of Routine
+        break
+    continueRoutine = False  # will revert to True if at least one component still running
     for thisComponent in trian_1Components:
-        if hasattr(thisComponent, 'status'):
-            thisComponent.status = NOT_STARTED
+        if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+            continueRoutine = True
+            break  # at least one component has not yet finished
     
-    # -------Start Routine "trian_1"-------
-    while continueRoutine:
-        # get current time
-        t = trian_1Clock.getTime()
-        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
-        # update/draw components on each frame
-        
-        # *beauty_left* updates
-        if t >= 0.0 and beauty_left.status == NOT_STARTED:
-            # keep track of start time/frame for later
-            beauty_left.tStart = t
-            beauty_left.frameNStart = frameN  # exact frame index
-            beauty_left.setAutoDraw(True)
-        
-        # *ugly_right* updates
-        if t >= 0.0 and ugly_right.status == NOT_STARTED:
-            # keep track of start time/frame for later
-            ugly_right.tStart = t
-            ugly_right.frameNStart = frameN  # exact frame index
-            ugly_right.setAutoDraw(True)
-        
-        # *or_hot* updates
-        if t >= 0.0 and or_hot.status == NOT_STARTED:
-            # keep track of start time/frame for later
-            or_hot.tStart = t
-            or_hot.frameNStart = frameN  # exact frame index
-            or_hot.setAutoDraw(True)
-        
-        # *image_train_1* updates
-        if t >= 0 and image_train_1.status == NOT_STARTED:
-            # keep track of start time/frame for later
-            image_train_1.tStart = t
-            image_train_1.frameNStart = frameN  # exact frame index
-            image_train_1.setAutoDraw(True)
-        
-        # *cross* updates
-        if t >= 0.0 and cross.status == NOT_STARTED:
-            # keep track of start time/frame for later
-            cross.tStart = t
-            cross.frameNStart = frameN  # exact frame index
-            cross.setAutoDraw(True)
-        
-        # *key_resp_2* updates
-        if t >= 0.0 and key_resp_2.status == NOT_STARTED:
-            # keep track of start time/frame for later
-            key_resp_2.tStart = t
-            key_resp_2.frameNStart = frameN  # exact frame index
-            key_resp_2.status = STARTED
-            # keyboard checking is just starting
-            win.callOnFlip(key_resp_2.clock.reset)  # t=0 on next screen flip
-            event.clearEvents(eventType='keyboard')
-        if key_resp_2.status == STARTED:
-            theseKeys = event.getKeys(keyList=['e', 'i'])
-            
-            # check for quit:
-            if "escape" in theseKeys:
-                endExpNow = True
-            if len(theseKeys) > 0:  # at least one key was pressed
-                key_resp_2.keys = theseKeys[-1]  # just the last key pressed
-                key_resp_2.rt = key_resp_2.clock.getTime()
-                # was this 'correct'?
-                if (key_resp_2.keys == str(ans)) or (key_resp_2.keys == ans):
-                    key_resp_2.corr = 1
-                else:
-                    key_resp_2.corr = 0
-                # a response ends the routine
-                continueRoutine = False
-        
-        # check if all components have finished
-        if not continueRoutine:  # a component has requested a forced-end of Routine
-            break
-        continueRoutine = False  # will revert to True if at least one component still running
-        for thisComponent in trian_1Components:
-            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
-                continueRoutine = True
-                break  # at least one component has not yet finished
-        
-        # check for quit (the Esc key)
-        if endExpNow or event.getKeys(keyList=["escape"]):
-            core.quit()
-        
-        # refresh the screen
-        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
-            win.flip()
+    # check for quit (the Esc key)
+    if endExpNow or event.getKeys(keyList=["escape"]):
+        core.quit()
     
-    # -------Ending Routine "trian_1"-------
-    for thisComponent in trian_1Components:
-        if hasattr(thisComponent, "setAutoDraw"):
-            thisComponent.setAutoDraw(False)
-    # check responses
-    if key_resp_2.keys in ['', [], None]:  # No response was made
-        key_resp_2.keys=None
-        # was no response the correct answer?!
-        if str(ans).lower() == 'none':
-           key_resp_2.corr = 1  # correct non-response
-        else:
-           key_resp_2.corr = 0  # failed to respond (incorrectly)
-    # store data for trials (TrialHandler)
-    trials.addData('key_resp_2.keys',key_resp_2.keys)
-    trials.addData('key_resp_2.corr', key_resp_2.corr)
-    if key_resp_2.keys != None:  # we had a response
-        trials.addData('key_resp_2.rt', key_resp_2.rt)
-    # the Routine "trian_1" was not non-slip safe, so reset the non-slip timer
-    routineTimer.reset()
-    thisExp.nextEntry()
-    
-# completed 1 repeats of 'trials'
+    # refresh the screen
+    if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+        win.flip()
 
-# get names of stimulus parameters
-if trials.trialList in ([], [None], None):
-    params = []
-else:
-    params = trials.trialList[0].keys()
-# save data for this loop
-trials.saveAsText(filename + 'trials.csv', delim=',',
-    stimOut=params,
-    dataOut=['n','all_mean','all_std', 'all_raw'])
+# -------Ending Routine "trian_1"-------
+for thisComponent in trian_1Components:
+    if hasattr(thisComponent, "setAutoDraw"):
+        thisComponent.setAutoDraw(False)
+# check responses
+if key_resp_2.keys in ['', [], None]:  # No response was made
+    key_resp_2.keys=None
+    # was no response the correct answer?!
+    if str("'e'").lower() == 'none':
+       key_resp_2.corr = 1  # correct non-response
+    else:
+       key_resp_2.corr = 0  # failed to respond (incorrectly)
+# store data for thisExp (ExperimentHandler)
+thisExp.addData('key_resp_2.keys',key_resp_2.keys)
+thisExp.addData('key_resp_2.corr', key_resp_2.corr)
+if key_resp_2.keys != None:  # we had a response
+    thisExp.addData('key_resp_2.rt', key_resp_2.rt)
+thisExp.nextEntry()
+# the Routine "trian_1" was not non-slip safe, so reset the non-slip timer
+routineTimer.reset()
 # these shouldn't be strictly necessary (should auto-save)
 thisExp.saveAsWideText(filename+'.csv')
 thisExp.saveAsPickle(filename)
